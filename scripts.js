14ee9a8ee4874d1d8c5ef295ac4bc5ee62389438
claudioccm <gigeer@gmail.com>
2014-11-06 13:10:35 -0500

    Cloned the Canvas files, and updated with the new questionaire. Manually indented all teh crap

---
+++ scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.tab').click(function() {
+    	console.log(this);
+        // var target = $(this).attr('data-target');
+        // $('.tab.m-active').removeClass('m-active');
+        // $('.form-section.m-active').removeClass('m-active');
+        // $(this).addClass('m-active');
+        // $(target).addClass('m-active');
+    });
+});
